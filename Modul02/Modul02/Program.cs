423e4242c83ae5979d0c31615d6faf5f938221ea
Dorian <dor1an@abv.bg>
2016-10-09 15:20:56 +0300

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul02
+{
+    class MainClass
+    {
+         public static void Main(string[] args)
+        {
+
+             //Дефинира на променливи
+
+
+             //Събиране на числа
+
+
+             //писане на променливи
+            
+            
+        }
+    }
+}
