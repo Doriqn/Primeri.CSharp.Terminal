f91c42ab640725a9d4a82cdc5af7255d5d617c0a
Dorian <dor1an@abv.bg>
2016-10-09 14:28:21 +0300

    Създаване на програма Hello Aula

---
+++ HelloAula/HelloAula/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloAula
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aula!");
+            Console.ReadKey();
+        }
+    }
+}
